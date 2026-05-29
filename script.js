7c92cc67c67fea7bf59ab2240bccc690033ca9cd
pavlenkoyurij-sudo <pavlenkoyurij@gmail.com>
2026-05-29 17:53:40 +0300

    Розширені Модали
    Added animation effects to modal opening and closing.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+ 
         function openModal(title, description, price, image) {
 
             document.getElementById("course-modal")
             .style.display = "flex";
+
+            setTimeout(() => {
+                modal.classList.add("show");//додаємо клас "show" привідкритті
+            }, 10);
 
             document.getElementById("modal-title")
             .innerHTML = title;
@@ -23,4 +28,21 @@
                 .style.display = "none";
             }   
         }
+                // функція закриття модального вікна
+        function closeModal(event) {
+            if (event.target.id === "course-modal") {
+                
+                const modal = document.getElementById("course-modal");
+
+                modal.classList.remove("show");
+
+                setTimeout(() => {
+                    modal.style.display = "none";
+                }, 300);
+            }   
+        }
+
+        const modal = document.getElementsById("course-modal");//змінна модал
+
+
 
